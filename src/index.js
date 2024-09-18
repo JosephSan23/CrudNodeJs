fb79bd9c591fef651da6229cb4b6b6335fb66204
Joseph Olarte <josephsantiago2345@gmail.com>
2024-09-18 10:34:13 -0500

    Aplicando la configuracion del servidor con el puerto 3000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+import express from "express"
+
+//Initialization
+const app = express();
+//Settings
+app.set('port', process.env.PORT || 3000);
+//Middlewares
+
+//Routes
+
+//Public files
+
+//Run Server
+app.listen(app.get('port'), () =>
+    console.log('Server listening on port', app.get('port')));
